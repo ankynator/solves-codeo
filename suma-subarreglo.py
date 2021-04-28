c68eca3afe14336b88a1d3d7339e8747189d7ac9
Vander Idme <ankynator@gmail.com>
2021-04-28 17:42:54 -0500

    logrando los 100 puntos en 0x25

--- suma-subarreglo.py
+++ suma-subarreglo.py
@@ -1,31 +1,26 @@
 n = input()
 input_array = input()
 array = [int(x) for x in input_array.split()]
-c = input()
-
 n = int(n)
+
+for i in range(1, n):
+    array[i] = array[i] + array[i-1]
+
+c = input()
 c = int(c)
-
 outputs = []
-
-
-def sumar(pq):
-    d = pq[1] - pq[0]
-    res = 0
-    i = pq[0]
-    while d >= 0:
-        res = res + array[i]
-        i = i+1
-        d = d-1
-
-    return res
-
 
 for i in range(0, c):
     response = 0
     input_pq = input()
     pq = [int(r) for r in input_pq.split()]
-    response = sumar(pq)
+    p, q = pq[0], pq[1]
+
+    if p == 0:
+        response = array[q]
+    else:
+        response = array[q] - array[p-1]
+
     outputs.append(response)
 
 for k in range(0, c):
