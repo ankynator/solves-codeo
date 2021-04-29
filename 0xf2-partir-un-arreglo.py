dff1f4111eea5dfdb512ec76c84a2b09cdacb633
Vander Idme <ankynator@gmail.com>
2021-04-29 00:11:30 -0500

    pass

--- 0xf2-partir-un-arreglo.py
+++ 0xf2-partir-un-arreglo.py
@@ -3,36 +3,18 @@ n = int(n)
 array_input = input()
 
 array = [int(x) for x in array_input.split()]
-left = array
-right = array[::-1]
+middles = []
 
-b_left = []
-b_right = []
 
-j = n - 1
-for i in range(0, n):
-    if i == 0:
-        left[i] = array[i]
-    else:
-        left[i] = left[i] + left[i-1]
-        right[i] = right[i] + right[i-1]
+for i in range(1, n):
+    array[i] = array[i] + array[i-1]
 
-    if left[i] > 0:
-        b_left.append(i)
-    if right[i] < 0:
-        b_right.append(j)
-    j -= 1
+for i in range(1, n):
+    if array[i-1] > 0 and (array[n - 1] - array[i - 1]) < 0:
+        middles.append(i)
 
-b_right = b_right[::-1]
 
-if b_right and b_left:
-    c = 0
-    while c < len(b_right):
-        if b_right[c] in b_left:
-            print(b_right[c])
-            break
-        else:
-            print('Impossible')
-        c += 1
+if middles:
+    print(middles[0])
 else:
     print('Impossible')
